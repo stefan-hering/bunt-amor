baf7aec3bf30deb5a27f1c494f41b44d0e39f3a8
stefan-hering <stefanvhering@gmail.com>
2016-06-26 21:59:18 +0200

    Made Photoswipe work

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 $(document).foundation();
+
+
+var pswpElement = document.querySelectorAll('.pswp')[0];
+
+// build items array
+var items = [];
+
+for(var i = 1; i <= 12; i++){
+	items.push({
+		src: '/img/galleries/p' + (i < 10 ? '0' : '') + i + '.JPG',
+        w: 3000,
+        h: 2000
+	});
+}
+
+// define options (if needed)
+var options = {
+    // optionName: 'option value'
+    // for example:
+    index: 0 // start at first slide
+};
+
+// Initializes and opens PhotoSwipe
+var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+gallery.init();
